c639a09b18537b705f22f4e0f44116c952d4ffca
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-01-30 18:03:58 -0300

    refatorando

--- src/main/java/valter/gabriel/Easy/Manager/security/WebConfig.java
+++ src/main/java/valter/gabriel/Easy/Manager/security/WebConfig.java
@@ -19,7 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
 
