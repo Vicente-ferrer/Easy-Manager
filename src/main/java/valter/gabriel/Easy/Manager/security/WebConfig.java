87571ee41586e80ee4f73eed8d83fdc6eb93c055
Valter Gabriel <63808405+ValterGabriell@users.noreply.github.com>
2023-01-30 16:20:24 -0300

    refatorando

--- src/main/java/valter/gabriel/Easy/Manager/security/WebConfig.java
+++ src/main/java/valter/gabriel/Easy/Manager/security/WebConfig.java
@@ -19,26 +19,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedOrigins("*");
+    }
+}
+
+/*
+ @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
-
-    /*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(ImmutableList.of("*"));
-        configuration.setAllowedMethods(ImmutableList.of("GET", "PUT", "POST", "DELETE", "PATCH"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(ImmutableList.of("*"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-     */
-}
+ */
